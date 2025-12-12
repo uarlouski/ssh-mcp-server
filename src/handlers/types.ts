82225f5a250e7dda869dcea40fe575fd5fbe938a
Uladzislau Arlouski <uarlouski@gmail.com>
2025-12-12 10:30:49 +0300

    Remove unused source files

--- src/handlers/types.ts
+++
@@ -1,7 +0,0 @@
-import type { SSHConnectionManager } from '../ssh-manager.js';
-import type { ConfigManager } from '../config.js';
-
-export interface HandlerContext {
-  sshManager: SSHConnectionManager;
-  configManager: ConfigManager;
-}
